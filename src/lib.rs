88036387261c63ecff7ec7bd179d1ef82af2ae49
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-09-14 18:48:08 -0400

    Added Black and White Player enum.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,43 @@
 pub mod game;
 pub mod io;
 
-pub use game::session::Session;
+pub use game::game::Game;
+
+
+// black and white are the players
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Player {
+    Black,
+    White,
+}
+
+impl Player {
+    // shows the player as a character
+    pub fn as_char(self) -> char {
+        match self {
+            Player::Black => 'B',
+            Player::White => 'W',
+        }
+    }
+
+    // chooses the next player to play
+    pub fn next(self) -> Self{
+        match self {
+            Player::Black => Player::White,
+            Player::White => Player::Black,
+        }
+    }
+
+    // shows the winner of the game
+    pub fn winner(self) -> String {
+        match self {
+            Player::Black => "Black".to_string(),
+            Player::White => "White".to_string(),
+        }
+    }
+
+}
+
+// the board of the game
+pub struct Board {
+}
