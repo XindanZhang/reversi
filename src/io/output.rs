85f0d5ee5034d2a9b5c70c0f62a990e55743caac
Xindan Zhang <xindan.zhang@mail.utoronto.ca>
2025-09-15 02:02:50 -0400

    Fixed winner output to display winner's name.

--- src/io/output.rs
+++ src/io/output.rs
@@ -4,7 +4,7 @@ pub fn winner_output(winner: Player, board: &Board){
     let points = winner.winner_points(board);
     match points {
         0 => println!("Draw!"),
-        _ => println!("{} wins by {} points!", winner.as_char(), points),
+        _ => println!("{} wins by {} points!", winner.winner(), points),
     }
 }
 
